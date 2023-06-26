1b1486760e31645a72d842cac19ba895e3436ab6
lyokocode <aelita.dev@gmail.com>
2023-06-26 07:31:16 +0300

    The private route structure has been created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AboutPage, BlogPage, HomePage, LoginPage } from './pages'
 import { Navbar } from "./components"
+import PrivateRoutes from '../utils/PrivateRoute'
 
 function App() {
-
 
   return (
     <div className=' w-full h-screen '>
       <Router>
         <Navbar />
         <Routes>
-          <Route element={<HomePage />} path="/" />
-          <Route element={<AboutPage />} path="/about" />
-          <Route element={<BlogPage />} path="/blogs" />
+          <Route element={<PrivateRoutes />}>
+            <Route element={<HomePage />} path="/" exact />
+            <Route element={<BlogPage />} path="/blogs" exact />
+          </Route>
           <Route element={<LoginPage />} path="/login" />
+          <Route element={<AboutPage />} path="/about" exact />
         </Routes>
       </Router>
     </div>
